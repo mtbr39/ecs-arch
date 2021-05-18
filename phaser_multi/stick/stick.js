f6f49981eee5a0ba080ca6a0a2ec898e4102fdea
mtbr39 <nm1552jp@gmail.com>
2021-05-19 02:38:06 +0900

    input ; drag and touch xy

--- phaser_multi/stick/stick.js
+++ phaser_multi/stick/stick.js
@@ -1,5 +1,7 @@
 //python -m http.server 8000
 //python -m SimpleHTTPServer 8000
+//タッチイベント  https://rexrainbow.github.io/phaser3-rex-notes/docs/site/touchevents/
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -37,18 +39,35 @@ function preload() {
 function create() {
     
     platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, "square").setScale(2).refreshBody();
+    platforms.create(400, 568, "square").setScale(0.5).refreshBody();
     platforms.create(600, 400, "square");
 
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '64px', fill: '#f00' });
 
     player = this.physics.add.sprite(100, 450, "circle");
 
+    stick = this.physics.add.sprite(200, 450, "circle").setScale(0.5);
 
+    stick
+    .setInteractive({ draggable: true })
+    .on("dragstart", function (pointer, dragX, dragY) {
+        // ...
+    })
+    .on("drag", function (pointer, dragX, dragY) {
+        stick.setPosition(dragX, dragY);
+    })
+    .on("dragend", function (pointer, dragX, dragY, dropped) {
+        // ...
+    });
 
 
 }
 
 function update() {
-    
+    var pointer = this.input.activePointer;
+    if (pointer.isDown) {
+        var touchX = pointer.x;
+        var touchY = pointer.y;
+        console.log(touchX, touchY);
+    }
 }
