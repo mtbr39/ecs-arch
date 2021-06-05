f3e75cf93f873963f00010db4e3a79a423dfb3b3
mtbr39 <nm1552jp@gmail.com>
2021-06-05 19:22:15 +0900

    credit-card-checkout first

---
+++ frontend_snippets/daily_ui/credit-card-checkout/vue.js
@@ -0,0 +1,12 @@
+const mochi_menu = new Vue({
+    el: "credit-card-checkout",
+    data: {
+        active_menu: 0,
+    },
+    methods: {
+        menu_click(n) {},
+    },
+    computed: {
+        position_class() {},
+    },
+});
