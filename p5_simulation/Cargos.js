0733e5fad4ff43cd2e9d04547c772f2bb885fd3b
mtbr39 <nm1552jp@gmail.com>
2021-09-14 23:16:11 +0900

    p5: Cargo

---
+++ p5_simulation/Cargos.js
@@ -0,0 +1,36 @@
+class Cargos {
+    constructor() {
+        this.group = new Group();
+        this.cargos = [];
+    }
+    draw() {
+        this.cargos.forEach((cargoIns) => {
+            cargoIns.draw();
+        });
+    }
+    onMousePressed() {
+        let cargo = new Cargo({ x: mouseX, y: mouseY });
+        this.group.add(cargo.sprite);
+        this.cargos.push(cargo);
+    }
+}
+class Cargo {
+    constructor(options) {
+        let x0 = options.x | 0;
+        let y0 = options.y | 0;
+        this.sprite = createSprite(x0, y0, 40, 40);
+        this.sprite.shapeColor = color(200);
+        // this.sprite.velocity.y = 4.0;
+        this.nth = trace1.pushChildAndGetNth(); //todo : 付替え可能なTraceを所有させる
+    }
+    draw() {
+        this.sprite.velocity.x =
+            (trace1.getElderPosition(this.nth).x - this.sprite.position.x) *
+            0.02;
+        this.sprite.velocity.y =
+            (trace1.getElderPosition(this.nth).y - this.sprite.position.y) *
+            0.02;
+
+        trace1.updateNthPosition(this.nth, this.sprite.position);
+    }
+}
