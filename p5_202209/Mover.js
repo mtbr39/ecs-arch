dd7d1b6dda7e1f53184665ac7444761af42af465
mtbr39 <nm1552jp@gmail.com>
2022-10-21 09:19:54 +0900

    Vision

--- p5_202209/Mover.js
+++ p5_202209/Mover.js
@@ -2,19 +2,44 @@ class Mover {
     constructor() {
         this.sprite = createSprite(width / 2, height / 2, 40, 40);
         this.sprite.visible = false;
-        this.sprite.velocity.x = 2.0;
+        // this.sprite.velocity.x = 2.0;
+        this.sprite.addSpeed(0.2, this.sprite.rotation);
+        this.vision = new Vision({ x: this.sprite.position.x, y: this.sprite.position.y, offsetX: this.sprite.width});
         drawer.submitObject(this);
-        console.log(this.sprite, this.sprite.y);
     }
     draw() {
+        this.vision.fixPosition(this.sprite.position.x, this.sprite.position.y);
+
         fill(0,0,0,1);
         strokeWeight(1.0);
         stroke(255);
         rect(this.sprite.position.x - this.sprite.width / 2, this.sprite.position.y - this.sprite.height / 2, this.sprite.width, this.sprite.height);
-        rect(40, 40, 60, 60);
         
     }
     somefunciton() {
 
+    }
+}
+
+class Vision {
+    constructor(options) {
+        let x0 = options.x | 0;
+        let y0 = options.y | 0;
+        this.offsetX = options.offsetX | 0;
+        this.offsetY = options.offsetY | 0;
+        this.sprite = createSprite(x0 + this.offsetX, y0 + this.offsetY, 200, 160);
+        this.sprite.visible = false;
+        // this.sprite.velocity.x = 2.0;
+        drawer.submitObject(this);
+    }
+    draw() {
+        fill(0, 0, 0, 1);
+        strokeWeight(1.0);
+        stroke(255);
+        rect(this.sprite.position.x - this.sprite.width / 2, this.sprite.position.y - this.sprite.height / 2, this.sprite.width, this.sprite.height);
+    }
+    fixPosition(x, y) {
+        this.sprite.position.x = x + this.offsetX;
+        this.sprite.position.y = y + this.offsetY;
     }
 }
