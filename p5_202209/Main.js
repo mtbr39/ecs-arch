658bded7338ed39b06cdb4978d8567dc326db8d9
mtbr39 <nm1552jp@gmail.com>
2022-10-23 11:18:07 +0900

    p5 play version 3

--- p5_202209/Main.js
+++ p5_202209/Main.js
@@ -7,7 +7,11 @@ let trace1;
 
 function setup() {
     createCanvas(1080, 720);
+    // world.gravity.y = 10;
     console.log("setup");
+
+    drawingContext.shadowBlur = 16;
+    drawingContext.shadowColor = color(255);
 
     drawer = new Drawer();
     mover = new Mover();
@@ -16,6 +20,10 @@ function setup() {
 
 function draw() {
     background(0);
+
+    fill(0, 0, 0, 1);
+    strokeWeight(1.0);
+    stroke(255);
 
     drawer.draw();
 
