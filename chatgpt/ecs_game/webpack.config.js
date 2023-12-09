cbb7d3dec8b5ffd55f8600ce1fc7ce1c51a8f94b
mtbr39 <nm1552jp@gmail.com>
2023-12-09 20:36:42 +0900

    webpack watch

--- chatgpt/ecs_game/webpack.config.js
+++ chatgpt/ecs_game/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/main.ts', // まとめたいファイルのエントリーポイント
+  mode: 'development', // モードを設定（developmentまたはproduction）
+  entry: './js/main.ts', // TypeScriptファイルのエントリーポイント
   output: {
-    filename: 'bundle.js', // バンドルされたファイル名
+    filename: 'bundle.js', // 出力ファイルの名前
     path: path.resolve(__dirname, 'dist') // 出力先ディレクトリ
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // TypeScriptファイルを対象にする
+        use: 'ts-loader', // ts-loaderを使ってTypeScriptを処理する
+        exclude: /node_modules/ // node_modulesディレクトリは除外する
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'] // import時の拡張子省略
   }
 };
