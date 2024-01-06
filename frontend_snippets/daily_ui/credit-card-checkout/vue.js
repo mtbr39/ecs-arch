fb99d4207b9e7707f448da0e33ae594532133a71
mtbr39 <nm1552jp@gmail.com>
2024-01-06 17:47:23 +0900

    file

--- frontend_snippets/daily_ui/credit-card-checkout/vue.js
+++
@@ -1,9 +0,0 @@
-const credit_card_checkout = new Vue({
-    el: "#credit-card-checkout",
-    data: {
-        card_number: "",
-        card_holder: "",
-        expiry_date: "",
-        card_cvc: "",
-    },
-});
--- p5_202209/Utl39.js
+++
@@ -1,5 +0,0 @@
-class Utl39 { 
-    static random(max=100) { 
-        return Math.floor(Math.random() * max);
-    }
-}
